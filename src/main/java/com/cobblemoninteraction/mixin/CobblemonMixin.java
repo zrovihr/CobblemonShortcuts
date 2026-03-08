b459ac4b3ba8710de8c14cfd21a32ab4bba3477c
Zan Zrov <amirullahizzan@gmail.com>
2026-03-08 09:35:03 +0700

    Initial commit

---
+++ src/main/java/com/cobblemoninteraction/mixin/CobblemonMixin.java
@@ -0,0 +1,15 @@
+package com.cobblemoninteraction.mixin;
+
+import net.minecraft.server.MinecraftServer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(MinecraftServer.class)
+public class CobblemonMixin {
+	@Inject(at = @At("HEAD"), method = "loadLevel")
+	private void onTargetMethod(CallbackInfo info) {
+		// This code is injected into the start of MinecraftServer.loadLevel()V
+	}
+}
